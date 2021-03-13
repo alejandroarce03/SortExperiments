6b1d98e76023947362b29cf24d53c212a4c15624
Gevorah <jhonsebastianijajiortiz@gmail.com>
2021-03-12 21:34:33 -0500

    final commit

--- SortExperiments/SortExperiments/SortExperiments/UI/Interface.cs
+++ SortExperiments/SortExperiments/SortExperiments/UI/Interface.cs
@@ -166,10 +166,9 @@ namespace SortExperiments.UI
                             Cases(value);
                             repetition++;
                         }
-               
-                        break;
 
-                } Console.WriteLine(count[0] + "  " + count[1] + "  " + count[2] + "  " + count[3] + "  " + count[4] + "  " + count[5] + "  " + count[6] + "  " + count[7] + "  " + count[8] + "  " + count[9] + "  " + count[10] + "  " + count[11]);
+                        break;
+                }
             }
         }
 
